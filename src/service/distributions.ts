69fc1bd7a15c5f466b2ecd706ffcb528f33ca0a3
phi.nt <phi.nt@briswell.com>
2018-10-18 17:28:51 +0700

    I### Changed - 配給系のAPIを追加

--- src/service/distributions.ts
+++ src/service/distributions.ts
@@ -1,4 +1,4 @@
-import { OK } from 'http-status';
+import { CREATED, NO_CONTENT, OK } from 'http-status';
 
 import * as factory from '../factory';
 import { Service } from '../service';
@@ -15,6 +15,53 @@ export class DistributionsService extends Service {
             expectedStatusCodes: [OK]
         }).then(async (response) => {
             return response.json();
+        });
+    }
+    public async createDistribution(params: {
+        id: string;
+        name: string;
+    }): Promise<factory.distributions.distribute.IDistributions> {
+        return this.fetch({
+            uri: '/distributions/add',
+            method: 'POST',
+            body: params,
+            expectedStatusCodes: [CREATED]
+        }).then(async (response) => response.json());
+    }
+    public async searchDistribution(params: factory.distributions.distribute.ISearchConditions): Promise<{
+        totalCount: number;
+        data: factory.distributions.distribute.IDistributions[];
+    }> {
+        return this.fetch({
+            uri: '/distributions/search',
+            method: 'GET',
+            qs: params,
+            expectedStatusCodes: [OK]
+        }).then(async (response) => {
+            return {
+                totalCount: Number(<string>response.headers.get('X-Total-Count')),
+                data: await response.json()
+            };
+        });
+    }
+    public async updateDistribution(params: {
+        id: string;
+        name: string;
+    }): Promise<void> {
+        await this.fetch({
+            uri: `/distributions/${params.id}`,
+            method: 'PUT',
+            body: { name: params.name },
+            expectedStatusCodes: [NO_CONTENT]
+        });
+    }
+    public async deleteDistribution(params: {
+        id: string;
+    }): Promise<void> {
+        await this.fetch({
+            uri: `/distributions/${params.id}`,
+            method: 'DELETE',
+            expectedStatusCodes: [NO_CONTENT]
         });
     }
 }
