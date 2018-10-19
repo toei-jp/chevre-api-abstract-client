1d0d78fee319f2c66f2565ff1ced10f7500d8594
thanhBriswell <thanh.clg@briswell.com>
2018-10-19 10:49:30 +0700

    ###added 興行区分サービスを追加

--- src/service/boxOfficeType.ts
+++ src/service/boxOfficeType.ts
@@ -1,4 +1,4 @@
-import { OK } from 'http-status';
+import { CREATED, NO_CONTENT, OK } from 'http-status';
 
 import * as factory from '../factory';
 import { Service } from '../service';
@@ -17,5 +17,52 @@ export class BoxOfficeTypeService extends Service {
             method: 'GET',
             expectedStatusCodes: [OK]
         }).then(async (response) => response.json());
+    }
+    public async createBoxOfficeType(params: {
+        id: string;
+        name: string;
+    }): Promise<factory.boxOfficeType.IBoxOfficeType> {
+        return this.fetch({
+            uri: '/boxOfficeTypes/add',
+            method: 'POST',
+            body: params,
+            expectedStatusCodes: [CREATED]
+        }).then(async (response) => response.json());
+    }
+    public async searchBoxOfficeType(params: factory.boxOfficeType.ISearchConditions): Promise<{
+        totalCount: number;
+        data: factory.boxOfficeType.IBoxOfficeType[];
+    }> {
+        return this.fetch({
+            uri: '/boxOfficeTypes/search',
+            method: 'GET',
+            qs: params,
+            expectedStatusCodes: [OK]
+        }).then(async (response) => {
+            return {
+                totalCount: Number(<string>response.headers.get('X-Total-Count')),
+                data: await response.json()
+            };
+        });
+    }
+    public async updateBoxOfficeType(params: {
+        id: string;
+        name: string;
+    }): Promise<void> {
+        await this.fetch({
+            uri: `/boxOfficeTypes/${params.id}`,
+            method: 'PUT',
+            body: { name: params.name },
+            expectedStatusCodes: [NO_CONTENT]
+        });
+    }
+    public async deleteBoxOfficeType(params: {
+        id: string;
+    }): Promise<void> {
+        await this.fetch({
+            uri: `/boxOfficeTypes/${params.id}`,
+            method: 'DELETE',
+            expectedStatusCodes: [NO_CONTENT]
+        });
     }
 }
