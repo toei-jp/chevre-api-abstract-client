7eedb0bc2cb4985fbd64f40615cbc54e4250c528
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-11-20 07:19:15 +0000

    ### Removed - 不要なインターフェースを削除

--- src/service/creativeWork.ts
+++ src/service/creativeWork.ts
@@ -45,16 +45,6 @@ export class CreativeWorkService extends Service {
         }).then(async (response) => response.json());
     }
 
-    public async getMovieRatingByIdentifier(params: {
-        identifier: string;
-    }): Promise<factory.creativeWork.movie.ICreativeWorkRating> {
-        return this.fetch({
-            uri: `/creativeWorks/movie/getRating/${params.identifier}`,
-            method: 'GET',
-            expectedStatusCodes: [OK]
-        }).then(async (response) => response.json());
-    }
-
     public async updateMovie(params: factory.creativeWork.movie.ICreativeWork): Promise<void> {
         await this.fetch({
             uri: `/creativeWorks/movie/${params.identifier}`,
