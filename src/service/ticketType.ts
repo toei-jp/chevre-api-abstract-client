0ad7db5731ad10023a2634fa244b6d92d682a0b9
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2018-11-26 00:12:22 +0000

    # CHEVRE-79 ### Changed - 券種グループ検索条件強化

--- src/service/ticketType.ts
+++ src/service/ticketType.ts
@@ -1,24 +1,7 @@
 import * as chevre from '@chevre/api-abstract-client';
-import { OK } from 'http-status';
-
-import * as factory from '../factory';
 
 /**
  * 券種サービス
  */
 export class TicketTypeService extends chevre.service.TicketType {
-    /**
-     * 関連券種グループ
-     */
-    public async getTicketTypeGroupList(params: {
-        ticketTypeId: string;
-    }): Promise<factory.ticketType.ITicketType[]> {
-        return this.fetch({
-            uri: `/ticketTypes/getTicketTypeGroupList/${params.ticketTypeId}`,
-            method: 'GET',
-            expectedStatusCodes: [OK]
-        }).then(async (response) => {
-            return response.json();
-        });
-    }
 }
